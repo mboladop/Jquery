b238c5eb6562a69849593f9710c3550186918c39
Luisa Bolado <mboladop@hotmail.com>
2018-04-27 15:54:53 +0000

    bulbs switches and slide challenges

---
+++ JQUERY/CARDS JQUERY/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ 
+ $(".bottom_button").click(function() {
+            $("#one").slideToggle(2000);
+             
+        })
+ $(".bottom_button").click(function() {
+        $("#two").fadeToggle(2000);
+ })
+              $("#three").slideToggle(2000);
+               $("#four").slideToggle(2000);
+                $("#five").slideToggle(2000);
+                 $("#six").slideToggle(2000);
+})
--- JQUERY/jquery.js
+++ JQUERY/jquery.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
-    $("#myParagraph").click(function(){
-        for (let i=1; i<=10; i++) {
-            $("#myParagraph").append("<p>This is paragraph number "+ i + "</p>"); 
+    $("#myParagraph").click(function() {
+        for (let i = 1; i <= 10; i++) {
+            $("#myParagraph").append("<p>This is paragraph number " + i + "</p>");
         }
     })
-
-
+    
+    $("img").click(function() {
+            $("[alt='change me']").attr("src", "../img/1.png");
+        })
+    
+   
+    $(".switches").click(function() {
+     
+       $(".switchOff1").attr("src", "../img/switch_on.png");
+       $(".light1").attr("src", "../imgbulb/bulbon.gif");
+       $(".switchOff2").attr("src", "../img/switch_on.png");
+       $(".light2").attr("src", "../imgbulb/bulbon.gif");
+       $(".switchOff3").attr("src", "../img/switch_on.png");
+       $(".light3").attr("src", "../imgbulb/bulbon.gif");
+        
+        
+    })    
+        
 })
